ff796dceaeda5b39882e353b08abc3f0a717ff42
woollybah <woollybah@e838c568-5c3d-11de-992f-fdd7e25b9163>
2009-07-03 20:45:11 +0000

    Some comment tweaks.

--- net.brucey.dltk.blitzmax.ui/src/net/brucey/dltk/blitzmax/ui/text/BlitzMaxScriptCommentScanner.java
+++ net.brucey.dltk.blitzmax.ui/src/net/brucey/dltk/blitzmax/ui/text/BlitzMaxScriptCommentScanner.java
@@ -3,21 +3,32 @@ package net.brucey.dltk.blitzmax.ui.text;
 import org.eclipse.dltk.compiler.task.ITodoTaskPreferences;
 import org.eclipse.dltk.ui.text.IColorManager;
 import org.eclipse.dltk.ui.text.ScriptCommentScanner;
+import org.eclipse.dltk.ui.text.TodoTagRule;
+import org.eclipse.dltk.ui.text.TodoTaskPreferencesOnPreferenceStore;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.rules.IRule;
 
 public class BlitzMaxScriptCommentScanner extends ScriptCommentScanner {
 
-  private static final char COMMENT_CHAR = '\'';
+	private static final char COMMENT_CHAR = '\'';
 
-  public BlitzMaxScriptCommentScanner(IColorManager manager,
-      IPreferenceStore store, String comment, String todoTag,
-      ITodoTaskPreferences preferences) {
-    super(manager, store, comment, todoTag, preferences);
-  }
+	public BlitzMaxScriptCommentScanner(IColorManager manager,
+	    IPreferenceStore store, String comment, String todoTag,
+	    ITodoTaskPreferences preferences) {
+		super(manager, store, comment, todoTag, preferences);
+	}
 
-  @Override
-  protected char getCommentChar() {
-    return COMMENT_CHAR;
-  }
+	@Override
+	protected char getCommentChar() {
+		return COMMENT_CHAR;
+	}
+
+	protected IRule createTodoRule() {
+		final ITodoTaskPreferences prefs = new TodoTaskPreferencesOnPreferenceStore(
+		    getPreferenceStore());
+		return new TodoTagRule(
+		    getToken(IBlitzMaxColorConstants.BLITZMAX_TODO_COMMENT), prefs
+		        .getTagNames(), prefs.isCaseSensitive());
+	}
 
 }
