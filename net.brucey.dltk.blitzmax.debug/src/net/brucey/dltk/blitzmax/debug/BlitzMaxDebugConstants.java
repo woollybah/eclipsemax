279bc99bf656842d80eec1e50ae6fcd77ca95e1a
woollybah <woollybah@e838c568-5c3d-11de-992f-fdd7e25b9163>
2009-07-05 01:56:49 +0000

    Initial Import - net.brucey.dltk.blitzmax.debug

---
+++ net.brucey.dltk.blitzmax.debug/src/net/brucey/dltk/blitzmax/debug/BlitzMaxDebugConstants.java
@@ -0,0 +1,12 @@
+package net.brucey.dltk.blitzmax.debug;
+
+public class BlitzMaxDebugConstants {
+
+	private BlitzMaxDebugConstants() {
+	}
+
+	public static final String DEBUG_MODEL_ID = "net.brucey.dltk.blitzmax.debug.blitzmaxModel"; //$NON-NLS-1$
+
+	public static final String DEBUGGING_ENGINE_ID_KEY = "debugging_engine_id"; //$NON-NLS-1$
+
+}
