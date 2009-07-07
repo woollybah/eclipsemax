a54e30c039d5ba43fb3c397e2d57491b02818c16
woollybah <woollybah@e838c568-5c3d-11de-992f-fdd7e25b9163>
2009-07-07 22:16:23 +0000

    Removed Interpreter Arguments widget from configuration.

---
+++ net.brucey.dltk.blitzmax.debug.ui/src/net/brucey/dltk/blitzmax/debug/ui/compilers/BlitzMaxArgumentsTab.java
@@ -0,0 +1,13 @@
+package net.brucey.dltk.blitzmax.debug.ui.compilers;
+
+import org.eclipse.dltk.debug.ui.launchConfigurations.ScriptArgumentsTab;
+import org.eclipse.dltk.internal.debug.ui.launcher.InterpreterArgumentsBlock;
+
+public class BlitzMaxArgumentsTab extends ScriptArgumentsTab {
+
+  @Override
+  protected InterpreterArgumentsBlock createInterpreterArgsBlock() {
+    return null;
+  }
+
+}
--- net.brucey.dltk.blitzmax.debug.ui/src/net/brucey/dltk/blitzmax/debug/ui/launchConfigurations/BlitzMaxTabGroup.java
+++ net.brucey.dltk.blitzmax.debug.ui/src/net/brucey/dltk/blitzmax/debug/ui/launchConfigurations/BlitzMaxTabGroup.java
@@ -1,5 +1,6 @@
 package net.brucey.dltk.blitzmax.debug.ui.launchConfigurations;
 
+import net.brucey.dltk.blitzmax.debug.ui.compilers.BlitzMaxArgumentsTab;
 import net.brucey.dltk.blitzmax.debug.ui.compilers.BlitzMaxCompilerTab;
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
@@ -7,14 +8,13 @@ import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.EnvironmentTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
-import org.eclipse.dltk.debug.ui.launchConfigurations.ScriptArgumentsTab;
 
 public class BlitzMaxTabGroup extends AbstractLaunchConfigurationTabGroup {
   public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
     BlitzMaxMainLaunchConfigurationTab main = new BlitzMaxMainLaunchConfigurationTab(
         mode);
     ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] { main,
-        new ScriptArgumentsTab(), new BlitzMaxCompilerTab(),
+        new BlitzMaxArgumentsTab(), new BlitzMaxCompilerTab(),
         new EnvironmentTab(), new CommonTab() };
 
     setTabs(tabs);
