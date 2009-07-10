66c6c2c18e5a6c5384a69407e49b2e00b2b57d75
samahnub <samahnub@e838c568-5c3d-11de-992f-fdd7e25b9163>
2009-07-10 14:13:17 +0000

    Fixed content type so file icons will work.  Originally it was only changed in one place and was inadvertently rolled back.

--- net.brucey.dltk.blitzmax.core/src/net/brucey/dltk/blitzmax/core/BlitzMaxLanguageToolkit.java
+++ net.brucey.dltk.blitzmax.core/src/net/brucey/dltk/blitzmax/core/BlitzMaxLanguageToolkit.java
@@ -8,29 +8,30 @@ import org.eclipse.dltk.core.IDLTKLanguageToolkit;
 public class BlitzMaxLanguageToolkit extends AbstractLanguageToolkit implements
     IDLTKLanguageToolkit {
 
-	private static BlitzMaxLanguageToolkit toolkit;
+  private static BlitzMaxLanguageToolkit toolkit;
 
-	public static IDLTKLanguageToolkit getDefault() {
-		if (toolkit == null) {
-			toolkit = new BlitzMaxLanguageToolkit();
-		}
-		return toolkit;
-	}
+  public static IDLTKLanguageToolkit getDefault() {
+    if (toolkit == null) {
+      toolkit = new BlitzMaxLanguageToolkit();
+    }
+    return toolkit;
+  }
 
-	public String getLanguageContentType() {
-		return "net.brucey.dltk.blitzmax.content-type";
-	}
+  public String getLanguageContentType() {
+    return "net.brucey.dltk.blitzmax.contentType";
+  }
 
-	public String getLanguageName() {
-		return "BlitzMax";
-	}
+  public String getLanguageName() {
+    return "BlitzMax";
+  }
 
-	public String getNatureId() {
-		return BlitzMaxNature.BLITZMAX_NATURE;
-	}
+  public String getNatureId() {
+    return BlitzMaxNature.BLITZMAX_NATURE;
+  }
 
-	public String getPreferenceQualifier() {
-		return BlitzMaxCorePlugin.PLUGIN_ID;
-	}
+  @Override
+  public String getPreferenceQualifier() {
+    return BlitzMaxCorePlugin.PLUGIN_ID;
+  }
 
 }
